c61270193ffc740cf9938c6687fafebfc5acc927
freesummerwind <liz-e-tta@yandex.ru>
2023-05-20 20:36:08 +0300

    fix class Group

--- Boyarkina/data_collector.py
+++ Boyarkina/data_collector.py
@@ -5,17 +5,38 @@ class Group:
     Других функций у класса нет, нужен для более удобного представления группы
     """
 
-    def __init__(self, group_id, name, features, theme):
+    def __init__(self, group_id, name, status, description, group_type, activity, can_make_post, can_suggest_post,
+                 main_section, subscribers_number, is_verified, photo_url, posts, theme):
         """
         Конструктор класса
         :param group_id: идентификатор группы, int
         :param name: название группы, string
-        :param features: собранные фичи для группы, массив упорядоченных фич
+        :param status: статус группы, string
+        :param description: описание группы, string
+        :param group_type: тип группы, string
+        :param activity: базовая активность группы, string
+        :param can_make_post: может ли пользователь создать пост, int
+        :param can_suggest_post: может ли пользователь предложить пост, int
+        :param main_section: главная секция группы, int
+        :param subscribers_number: число подписчиков, int
+        :param is_verified: верифицирована ли группа, int
+        :param photo_url: ссылка на главное фото группы, string
+        :param posts: информация о последних 15 записях группы, массив объектов класса Post
         :param theme: тематика группы, string
         """
         self.__id = group_id
         self.__name = name
-        self.__features = features
+        self.__status = status
+        self.__desc = description
+        self.__type = group_type
+        self.__activity = activity
+        self.__can_make_post = can_make_post
+        self.__can_suggest_post = can_suggest_post
+        self.__main_section = main_section
+        self.__subscribers = subscribers_number
+        self.__is_verified = is_verified
+        self.__photo_url = photo_url
+        self.__last_15_posts = posts
         self.__theme = theme
 
     def get_id(self):
@@ -24,10 +45,22 @@ class Group:
     def get_name(self):
         return self.__name
 
-    def get_thema(self):
-        return self.__features
+    def get_features(self):
+        return {
+            'status': self.__status,
+            'description': self.__desc,
+            'type': self.__type,
+            'activity': self.__activity,
+            'can_make_post': self.__can_make_post,
+            'can_suggest_post': self.__can_suggest_post,
+            'main_section': self.__main_section,
+            'subscribers_number': self.__subscribers,
+            'is_verified': self.__is_verified,
+            'photo_url': self.__photo_url,
+            'posts_15': self.__last_15_posts
+        }
 
-    def get_interests(self):
+    def get_theme(self):
         return self.__theme
 
 
